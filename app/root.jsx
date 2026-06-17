a2f29ae57cb8a0cbc68834db0bd42b8e3895b410
Alexander Gubernikoff <agubernikoff@gmail.com>
2026-06-17 10:27:29 -0400

    linkedin tag

--- app/root.jsx
+++ app/root.jsx
@@ -227,6 +227,21 @@ export function Layout({children}) {
         </PopUpProvider>
         <ScrollRestoration nonce={nonce} />
         <Scripts nonce={nonce} />
+        <script
+          nonce={nonce}
+          dangerouslySetInnerHTML={{
+            __html: `var _linkedin_partner_id = "8628890"; window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || []; window._linkedin_data_partner_ids.push(_linkedin_partner_id); (function(l) { if (!l) { window.lintrk = function(a,b) { window.lintrk.q.push([a,b]) }; window.lintrk.q = [] } var s = document.getElementsByTagName("script")[0]; var b = document.createElement("script"); b.type = "text/javascript"; b.async = true; b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js"; s.parentNode.insertBefore(b, s); })(window.lintrk);`,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{display: 'none'}}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=8628890&fmt=gif"
+          />
+        </noscript>
       </body>
     </html>
   );
